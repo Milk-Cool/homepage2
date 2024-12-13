eba3e8314a047372fe08708d0991ab6987682253
Milk-Cool <milan-pl@mail.ru>
2024-12-13 19:49:31 +0300

    accessibility fixes

--- autoletters.js
+++ autoletters.js
@@ -1,5 +1,6 @@
 for(const el of document.querySelectorAll("letters")) {
     const text = el.innerText;
     el.innerHTML = "";
+    el.setAttribute("title", text);
     insertLetters(el, text, el.getAttribute("height"));
 }
